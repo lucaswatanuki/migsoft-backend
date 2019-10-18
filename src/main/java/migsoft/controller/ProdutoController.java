4c98faed338f72b0da031ea1b087e2d21621a463
Lucas-Watanuki <lucas.watanuki@ibm.com>
2019-10-18 10:37:20 -0300

    deploy no heroku

--- src/main/java/migsoft/controller/ProdutoController.java
+++ src/main/java/migsoft/controller/ProdutoController.java
@@ -3,10 +3,13 @@ package migsoft.controller;
 import migsoft.model.ProdutoEntity;
 import migsoft.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/produto")
 public class ProdutoController {
@@ -15,21 +18,25 @@ public class ProdutoController {
     ProdutoService produtoService;
 
     @PostMapping("")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ProdutoEntity post(@RequestBody ProdutoEntity produtoEntity) {
         return produtoService.save(produtoEntity);
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public List<ProdutoEntity> getAll() {
         return produtoService.findAll();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ProdutoEntity getProdutoById(@PathVariable int id) {
         return produtoService.findById(id);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public void deleteProdutoById(@PathVariable int id) {
         produtoService.deleteById(id);
     }
